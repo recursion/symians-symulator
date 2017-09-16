9f2912f9e1e052f130a794a240de32223886a365
recursion <msymmes@gmail.com>
2017-09-15 20:05:18 -0400

    Added new tests.

--- test/symulator_test.exs
+++ test/symulator_test.exs
@@ -5,4 +5,17 @@ defmodule SymulatorTest do
     t = Symulator.create_world("World_1001")
     assert is_pid(t) == true
   end
+
+  test "lookup: returns :ok and the pid of an existing world" do
+    a = Symulator.create_world("World_1002")
+    {:ok, b} = Symulator.lookup("World_1002")
+    assert a == b
+  end
+  test "lookup: returns :error when the world is not found" do
+    assert Symulator.lookup("World_1004") == :error
+  end
+
+  test "getArea: returns a matrix of locations" do
+    assert 1 == 2
+  end
 end
